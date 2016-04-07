84b8d90dd7d04035f6bd5c6f9c25173f305e8cb4
Raydelto Hernandez <ray.hernandez@bluespurs.com>
2016-04-07 12:54:58 -0400

    Rendering the first React-Redux app

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import {combineReducers} from 'redux'
+import BookReducer from './reducer_books'
+
+const rootReducer =  combineReducers(
+  {
+    books: BookReducer
+  }
+);
+
+export default rootReducer;

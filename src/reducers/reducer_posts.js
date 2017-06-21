6f0fb637e399de5b0f42d8377aa70214ed14087d
Raydelto <raydelto@gmail.com>
2017-06-21 10:54:57 -0400

    Removing unreachable statements

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,12 +4,9 @@ export default function (state = INITIAL_STATE, action){
   switch (action.type) {
     case FETCH_POST:
       return { ... state, post: action.payload.data};
-      break;
     case FETCH_POSTS:
       return { ... state, all: action.payload.data};
-      break;
     default:
       return state;
-
   }
 }
